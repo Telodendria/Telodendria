b3e71c32fda23838c5a92cee20f085a7b301f905
Jordan Bancino <jordan@bancino.net>
2022-07-28 12:00:52 -0400

    Update license text.

--- src/include/Json.h
+++ src/include/Json.h
@@ -10,7 +10,7 @@
  * subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
+ * included in all copies or portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
