2d6b80a26e650a31f1448cd0197d3a7274aab6e2
Jordan Bancino <jordan@bancino.net>
2022-09-30 19:11:44 -0400

    Move man pages into a proper man directory

--- tools/env.sh
+++ tools/env.sh
@@ -4,4 +4,9 @@ find tools/bin -type f | while IFS= read -r tool; do
 	chmod +x "$tool"
 done
 
-PATH="$(pwd)/tools/bin:$PATH"
+makewhatis "$(pwd)/man"
+
+export PATH="$(pwd)/tools/bin:$PATH"
+export MANPATH="$(pwd)/man:$MANPATH"
+
+PS1="(td) $PS1"
