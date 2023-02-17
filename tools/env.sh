d81e8f3a324fe5773ef7eae463b03a9f88b96cb9
Jordan Bancino <jordan@bancino.net>
2023-02-17 18:26:08 +0000

    We're building up some tools; let's print them out in env.sh

--- tools/env.sh
+++ tools/env.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
 
-find tools/bin -type f | while IFS= read -r tool; do
+echo "Telodendria Development Environment"
+echo "-----------------------------------"
+echo
+echo "Tools available:"
+find tools/bin -type f -not -path '*/CVS/*' | while IFS= read -r tool; do
+    echo "- $(basename $tool)"
 	chmod +x "$tool"
 done
 
-makewhatis "$(pwd)/man"
+if which makewhatis 2>&1 > /dev/null; then
+    makewhatis "$(pwd)/man"
+fi
 
 export PATH="$(pwd)/tools/bin:$PATH"
 export MANPATH="$(pwd)/man:$MANPATH"
