012c334ee54528524c4584000fcc90aee9a85dba
Jordan Bancino <jordan@bancino.net>
2023-03-09 00:06:15 +0000

    Make send-patch use json instead of jq.

--- tools/env.sh
+++ tools/env.sh
@@ -8,9 +8,24 @@ find tools/bin -type f | grep -v CVS | grep -v '#' | while IFS= read -r tool; do
     echo "- $(basename $tool)"
 	chmod +x "$tool"
 done
-find tools/src -type f -name '*.c' | while IFS= read -r tool; do
-    echo "- $(basename $tool .c)"
+
+missing=0
+
+for tool in $(find tools/src -type f -name '*.c'); do
+    base=$(basename "$tool" .c)
+    printf "- $base"
+    if [ ! -f "build/tools/$base" ]; then
+        printf ' (missing)'
+        missing=1
+    fi
+    echo
 done
+
+if [ "$missing" -eq "1" ]; then
+    echo
+    echo "Warning: Some tools are missing, which means others"
+    echo "may not work properly. Build missing tools with td."
+fi
 
 if which makewhatis 2>&1 > /dev/null; then
     makewhatis "$(pwd)/man"
