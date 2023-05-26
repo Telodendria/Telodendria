d24c1161f62315569f4f5e679ed62ed137d48bdc
Jordan Bancino <jordan@bancino.net>
2023-05-26 22:25:03 +0000

    Add a Cytoplasm example, which I'll use to debug some stuff.

---
+++ Cytoplasm/examples/PrintArgsAndEnv.c
@@ -0,0 +1,27 @@
+#include <Array.h>
+#include <HashMap.h>
+
+#include <Log.h>
+
+int Main(Array *args, HashMap *env)
+{
+	size_t i;
+	char *key;
+	char *val;
+
+	Log(LOG_INFO, "Hello World!");
+	Log(LOG_INFO, "Arguments: %lu", ArraySize(args));
+
+	for (i = 0; i < ArraySize(args); i++)
+	{
+		Log(LOG_INFO, "  [%ld] %s", i, ArrayGet(args, i));
+	}
+
+	Log(LOG_INFO, "Environment:");
+	while (HashMapIterate(env, &key, (void **) &val))
+	{
+		Log(LOG_INFO, "  %s = %s", key, val);
+	}
+
+	return 0;
+}
